4a87659aa04d7b75360dcdf3dd68060a2f899250
Lesia <slinkova.albul@gmail.com>
2026-02-13 05:29:42 -0800

    Final blood cells model, metrics image and streamlit page

---
+++ models/model_data_blood_cells.py
@@ -0,0 +1,12 @@
+# models/model_metadata.py
+
+MODEL_INFO = {
+    "total_images": 9957,
+    "class_counts": {
+        "EOSINOPHIL": 2497,
+        "LYMPHOCYTE": 2483,
+        "MONOCYTE": 2478,
+        "NEUTROPHIL": 2499,
+    },
+    "final_f1": 0.82
+}
